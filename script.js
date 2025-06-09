4800fcfb9c2aad224780a9c653aba9b125efa48b
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-06-09 14:37:08 +0100

    Add explanation of conditional statements

---
+++ script.js
@@ -0,0 +1,12 @@
+// Conditional Statements
+
+// Conditional statements are used to perform different actions based on different conditions
+
+// Javascript conditional statements:
+// if (executes a block of code if condition is true)
+// else (executes a block of code if same condition is false)
+// else if (a new condition if first condition is false)
+// switch (many alternative blocks of code)
+// ? (inline conditional statement)
+
+// Conditionals are a core part of writing dynamic and interactive code
